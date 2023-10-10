af2147337abae0636660c70a2453fdc44c4da7f7
Vineet Singh Negi <137212462+VineetSnegi@users.noreply.github.com>
2023-10-10 22:14:17 +0530

    Added binary search file

---
+++ Binary_Search.py
@@ -0,0 +1,40 @@
+arr = [1, 3, 3, 4, 5, 6, 7, 8]
+
+# Python implementation of Binary Search
+def binarySearch(arr, target):
+    L, R = 0, len(arr) - 1
+
+    while L <= R:
+        mid = (L + R) // 2
+
+        if target > arr[mid]:
+            L = mid + 1
+        elif target < arr[mid]:
+            R = mid - 1
+        else:
+            return mid
+    return -1
+
+# Binary search on some range of values
+def binarySearch2(low, high):
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if isCorrect(mid) > 0:
+            high = mid - 1
+        elif isCorrect(mid) < 0:
+            low = mid + 1
+        else:
+            return mid
+    return -1
+
+# Return 1 if n is too big, -1 if n in too small, 0 if correct
+def isCorrect(n):
+    if n > 10:
+        return 1
+    elif n < 10:
+        return -1
+    else:
+        return 0
+    
